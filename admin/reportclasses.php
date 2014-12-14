809f54b08a02893d1df135bf8edf80a895f4bfb1
Justin Hunt <poodllsupport@gmail.com>
2014-12-14 10:07:52 +0900

    added some spam filter. spec. to us.

--- admin/reportclasses.php
+++ admin/reportclasses.php
@@ -440,6 +440,14 @@ class local_majhub_mailchimp_report extends  local_majhub_base_report {
 							$itsbad = true;
 						}
 					}
+					//if it is just bad because of crap spam blizzard annoyingness
+					$stubs = array('laposte.net','outlook.com');
+					foreach ($stubs as $stub){
+						if(strpos($user->email,'@' . $stub)){
+							$itsbad = true;
+						}
+					}
+					
 					if($itsbad){
 						//echo 'itsbad:' . $user->email . '<br />';
 						continue;
